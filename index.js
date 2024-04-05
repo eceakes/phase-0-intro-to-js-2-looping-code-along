596d9ce7558ed6955986f09bd2e1d5061e8f284b
Eric <eceakes@gmail.com>
2024-04-05 09:45:13 -0400

    message.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+function writeCards(names) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+}
+writeCards(names);
+console.log(writeCards(cards));
+
+function countDown() {
+    for (let countDown = 10; countDown <11; countDown--) {
+    while (countDown <11, countDown > -1) {
+        console.log(countDown --);
+    }
+}
+}
+countDown;
